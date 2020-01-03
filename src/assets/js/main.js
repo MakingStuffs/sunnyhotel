dd024ccdb2b5e6d2342e35695908a3a32cd82515
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-01-03 13:44:22 +0000

    Removed call to modal.js from main.js as modal is now a custom element

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,4 +1,7 @@
 import '../scss/main.scss';
-import './modal';
 
+const btn = document.querySelector('#contactButton');
+btn.addEventListener('click', () => {
+    document.querySelector('contact-modal').open = true;
+});
 module.hot.accept((err) => console.log(err));
--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -1,8 +1,7 @@
 'use strict';
 import 'babel-polyfill';
 import {
-    msQuery,
-    msQueryAll
+    msQuery
 } from 'making-stuffs-queries';
 
 const contactButton = msQuery('#contactButton');
@@ -26,18 +25,31 @@ function modalToggle() {
     1. regex for names and subject allowing letters, hyphens and spaces
     3. regex for email following as close to email structure as possible
     4. regex for textarea allowing normal grammatical characters
+    5. skip submit button
+    6. account for textarea elem
+    7. reset regex on each iteratin of loop
+    8. check if field is empty 
+    9. Check field against regex if it isn't empty
+    10. push errors to error array
+    11. if there are errors return the array
+    12. else return true (as in passed validation)
 */
 const validateFields = async (fieldset) => {
+
     const textRegex = /^[a-zA-Z]+(([ -][a-zA-Z ])?[a-zA-Z]*)*$/;
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    const textareaRegex = /^[a-zA-Z0-9]+(([',. -";:()€£$\$\!\?\/][a-zA-Z 0-9])?[a-zA-Z0-9]*)*$/;
+    const textareaRegex = /[{}<>]+/g;
     const fields = fieldset.children;
     let errors = [];
+
     for (let field of fields) {
+
         textRegex.lastIndex = 0;
         emailRegex.lastIndex = 0;
         textareaRegex.lastIndex = 0;
+
         if (field.type === 'submit') continue;
+
         if (!field.value) {
             errors.push({
                 field: field.name,
@@ -45,39 +57,103 @@ const validateFields = async (fieldset) => {
             });
             continue;
         }
+
         if (field.type === 'text' && !textRegex.test(field.value)) {
-            console.log(textRegex.test(field.value));
             errors.push({
                 field: field.name,
                 error: 'illegal character'
             });
             continue;
-        } else if (field.type === 'email' && !emailRegex.test(field.value)) {
+        } else
+
+        if (field.type === 'email' && !emailRegex.test(field.value)) {
             errors.push({
                 field: field.name,
                 error: 'illegal character'
             });
-        } else if (field.tagName === 'TEXTAREA' && !textareaRegex.test(field.value)) {
+        } else
+
+        if (field.tagName === 'TEXTAREA' && textareaRegex.test(field.value)) {
             errors.push({
                 field: field.name,
                 error: 'illegal character'
             });
             continue;
         }
+
     }
-    return errors ? errors : 'passed';
+
+    return errors[0] ? errors : true;
+}
+
+/*
+    Error handler
+    1. take an object of errors
+    2. find each error's corresponding element 
+    3. Highlight it in red
+    4. construct a message
+    5. append the message to the contact form
+    6. enable the contact form
+*/
+const errorHandler = async (errors) => {
+    const outputHead = msQuery('h3', msQuery('.outcome-head'));
+    const outputContent = msQuery('.outcome-content');
+    const outputClose = msQuery('.outcome-footer');
+    const output = msQuery('.outcome');
+    const blackout = msQuery('.blackout');
+    const fields = msQuery('fieldset');
+    const loader = msQuery('.loader');
+
+    loader.classList.remove('alive');
+    fields.disabled = true;
+    blackout.classList.remove('kill');
+    output.style = 'transform: translate(-50%, -50%);';
+    outputHead.style = 'background-color: #CA3C25;';
+    outputHead.textContent = 'There\'s been an error';
+    outputContent.innerHTML = '<p>The following elements have issues with their input: </p>';
+
+    for (let field of errors) {
+
+        if (field.field !== 'submit') {
+            let elem = msQuery(`[name="${field.field}"]`);
+            elem.style.boxShadow = '0 0 0 0.2rem red';
+        }
+
+        outputContent.innerHTML += `<p>${field.field}: <strong>${field.error}</strong></p>`;
+    }
+
+    const closeOutcome = () => {
+        const loader = msQuery('.loader');
+        fields.disabled = false;
+        output.style = 'transform: translate(-50%, -200%);';
+        loader.classList.remove('alive');
+        blackout.classList.add('kill');
+        outputClose.removeEventListener('click', closeOutcome);
+    }
+
+    outputClose.addEventListener('click', closeOutcome);
+}
+
+const successHandler = () => {
+
 }
 
 const submitHandler = async (e) => {
     e.preventDefault();
     const loader = msQuery('.loader');
+    const blackout = msQuery('.blackout');
     const fields = msQuery('fieldset');
     const modalContent = msQuery('.modal-content');
     const validation = await validateFields(fields);
-    console.log(validation);
-    modalContent.style.overflow = 'hidden';
+
+    modalContent.style.overflowY = 'hidden';
+    if (validation !== true) {
+        return errorHandler(validation)
+    };
+
     loader.classList.add('alive');
-    //fields.disabled = true;
+    blackout.classList.remove('kill');
+
     try {
         const reply = await fetch('http://localhost:8080/send-mail', {
             method: 'POST',
@@ -85,8 +161,10 @@ const submitHandler = async (e) => {
         });
         const data = await reply.json();
     } catch (err) {
-        loader.classList.remove('alive');
-        console.log(err);
+        return errorHandler([{
+            field: 'submit',
+            error: 'Server error'
+        }])
     }
 }
 
